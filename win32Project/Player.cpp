503d64e3e69ba1aa10d431dac8a454cf8c2f8dfb
Woojin Jo <Woojin.jo95@gmail.com>
2016-06-21 03:26:06 +0900

    add 점

--- win32Project/Player.cpp
+++ win32Project/Player.cpp
@@ -7,6 +7,7 @@ Player::Player()
 {
 	isGround = false;
 	isRide = false;
+	isJump = false;
 	//이미지 로드
 	this->img = new GImage(Renderer(), "./Resource/Action_img/Action_Character.bmp");
 	this->img2 = new GImage(Renderer(), "./Resource/Action_img/Action_Cart_Riding.bmp");
@@ -51,7 +52,7 @@ void Player::Initialize()
 
 void Player::Update()
 {
-
+	//깃발 위치에 있을 때
 	if (onFlag())
 	{
 		score = getY();
@@ -63,10 +64,31 @@ void Player::Update()
 		walk();
 	}
 		
+	//땅위에 있을 때
 	if (onGround())
 	{
 		jump();
 	}
+
+	//점프할때
+	if (isJump)
+	{
+		setY(getY() - 3);
+		if (getY() < 580)
+		{
+			isJump = false;
+		}
+	}
+	
+	if (!isJump&&!isGround)
+	{
+		setY(getY() + 3);
+		if (getY() > 620)
+		{
+			isJump = true;
+		}
+	}
+
 	if (IsKeyDown(VK_F2))
 	{
 		ride();
@@ -96,13 +118,11 @@ void Player::walk()
 
 void Player::jump()
 {
-	if (IsKeyDown(VK_UP))
+	if (getY() > 580 && IsKeyDown(VK_UP))
 	{
-		while (getY() < 580 )
-			setY(getY() + 3);
+		isJump = true;
 	}
 }
-
 
 void Player::ride()
 {
